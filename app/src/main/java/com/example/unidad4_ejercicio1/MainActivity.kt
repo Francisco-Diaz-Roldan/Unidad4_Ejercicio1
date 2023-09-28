8e93a9009aad39d95662171d9ef5bf895fc97ab4
FranciscoDíazRoldán <pacodiaz008@gmail.com>
2023-09-28 09:48:43 +0200

    Otros ajustes menores

--- app/src/main/java/com/example/unidad4_ejercicio1/MainActivity.kt
+++ app/src/main/java/com/example/unidad4_ejercicio1/MainActivity.kt
@@ -8,29 +8,28 @@ import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.Button
-import android.widget.DatePicker
 import android.widget.TextView
-import org.w3c.dom.Text
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity(),OnClickListener {
-    private var mYear=0
-    private var mMonth:Int = 0
-    private var mDay:Int = 0
+    private var mYear : Int =0
+    private var mMonth : Int = 0
+    private var mDay : Int = 0
 
     @SuppressLint("MissingInflatedId", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        //Establezco los valores para el TextView correspondiente al id tVFechaActual e indico el valor de la fecha local de hoy
         val fechaActual : TextView = findViewById(R.id.tVFechaActual)
         val hoy =LocalDate.now()
 
         // Formateo la fecha actual hoy y la paso al formato "día/mes/año"
         val formatoCorrecto = DateTimeFormatter.ofPattern("dd/MM/yyyy")
 
+        //Actualizo el valor del textView fechaActual con la fecha de hoy con el formato correcto
         fechaActual.text = "Hoy es ${hoy.format(formatoCorrecto)}"
 
         val boton=findViewById<Button>(R.id.btnFechaNac)
